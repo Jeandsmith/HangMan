8e8582c907fc02a07ae7688c60f6fbfd4631b429
James Smith Abreu <jeandsmith27@gmail.com>
2017-12-31 20:48:48 -0500

    Completed guessing algorithm

--- HangManGame/main.cpp
+++ HangManGame/main.cpp
@@ -2,18 +2,57 @@
 #include <fstream>
 
 void log(const char *msg); //To log messages to the screen
-void display_game_title(); // Game title
 void reset_buffer();
 const char *pick_word();
-const char get_user_input();
+const uint32_t word_length(const char *c);
 
 int main() {
 	srand(static_cast<uint32_t>(time(nullptr)));
+	log("HANGMAN \n");
+
+	const uint32_t LENGTH {word_length(pick_word())};
+	printf("%d \n", LENGTH);
+
+	char word_mask [LENGTH];
+	for (int i = 0; i < LENGTH; i++){
+		word_mask[i] = '-';
+	}
+
+	printf("Word:  %s \n", word_mask);
+//
+//	uint32_t player_lives = 3, guess = 0;
+//	const char * chosen_word = pick_word();
+//	while (player_lives != 0){
+//		++guess;
+//		printf("Enter guess: %d \n", guess);
+//		const char ch {static_cast<char>(toupper(getc(stdin)))};
+//		std::cin.ignore();
+//		for (int c = 0; chosen_word[c]; c++){
+//			if ( ch != chosen_word[c]
+//			    && c == word_length(chosen_word) - 1){
+//				puts("Not found");
+//				player_lives--;
+//				printf("Lives: %d \n", player_lives);
+//				reset_buffer();
+//				break;
+//			}
+//
+//			if (ch == chosen_word[c]){
+//				if (chosen_word[c] == word_mask[c]){
+//					continue;
+//				}
+//				word_mask[c] = chosen_word[c];
+//				printf("Word:  %s \n", word_mask);
+//				reset_buffer();
+//				break;
+//			}
+//		}
+//	}
 	return 0;
 }
 
 void log(const char *msg) {
-	fputs(msg, stdout);
+	puts(msg);
 	fflush(stdout);
 }
 
@@ -39,15 +78,22 @@ void reset_buffer() {
 }
 
 const char *pick_word() {
-	const uint32_t ARR_SIZE = 2;
-	const char * word_bank[] = {"BREAD", "PENGUINS"};
-	const char * word = word_bank[rand() % ARR_SIZE];
+	uint32_t arr_size = 0;
+	const char * word_bank[] = {"BREAD", "PENGUINS", "SAUSAGE", "WORLD", "CAR"};
+	for (const char *word: word_bank){
+		if (word == 0) break;
+		arr_size++;
+	}
+
+	const char * word = word_bank[rand() % arr_size];
 	return word;
 }
 
-const char get_user_input(){
-	puts("What is your guess? ");
-	char input {};
-	input = getc(stdin);
-	return input;
+const uint32_t word_length(const char *c){
+	uint32_t size {0};
+	for (; *c; c++){
+		size++;
+	}
+
+	return size;
 }
